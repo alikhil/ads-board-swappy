38ad8a24b42f63b09e692633e05a59050022108d
alikhil <alikhil@mail.ru>
2015-07-16 01:13:32 +0500

    Создан ASP MVC проект с модульными тестами

---
+++ Swappy-V2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Swappy_V2.Startup))]
+namespace Swappy_V2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
