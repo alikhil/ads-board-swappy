026cc88b7f1ae85f51d8b1308f561222de27e191
alikhil <alikhil@mail.ru>
2015-07-16 20:44:50 +0500

    ModelView для регистрации изменен - добавлены дополнительные поля­

--- Swappy-V2/Models/AccountViewModels.cs
+++ Swappy-V2/Models/AccountViewModels.cs
@@ -70,6 +70,33 @@ namespace Swappy_V2.Models
         public string Email { get; set; }
 
         [Required]
+        [Display(Name = "Имя")]
+        [MaxLength(50, ErrorMessage = "{0} должно иметь не более 50 символов")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string Surname { get; set; }
+
+        [Required]
+        [Display(Name = "Номер телефона")]
+        [RegularExpression(@"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Город")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Адрес электронной почты")]
+        public string Email { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
