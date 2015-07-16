1769f6dd2230a39a0b283e71da9fd8eddf7b253e
alikhil <alikhil@mail.ru>
2015-07-16 22:48:26 +0500

    Добавлен класс юзера - аналог Usert, убран userName[ибо зачем?]

---
+++ Swappy-V2/Models/AppUserModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Models
+{
+    public class AppUserModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Имя")]
+        [MaxLength(50, ErrorMessage = "{0} должно иметь не более 50 символов")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string Surname { get; set; }
+
+        [Required]
+        [Display(Name = "Номер телефона")]
+        [RegularExpression(@"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$", ErrorMessage = "Укажите корректный номер телефона")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Город")]
+        public string City { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Адрес электронной почты")]
+        public string Email { get; set; }
+
+    }
+}
