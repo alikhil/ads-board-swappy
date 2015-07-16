7fbf4784b65b6975c163be67310732997ba876dc
alikhil <alikhil@mail.ru>
2015-07-16 21:30:31 +0500

    Добавлены бандлы скриптов и стилей"

--- Swappy-V2/App_Start/BundleConfig.cs
+++ Swappy-V2/App_Start/BundleConfig.cs
@@ -9,10 +9,17 @@ namespace Swappy_V2
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kladr").Include(
+                "~/Scripts/kladr/jquery.kladr.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/myscripts").Include(
+                        "~/Scripts/myscripts.js"));
 
             // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
             // используйте средство сборки на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
@@ -26,6 +33,9 @@ namespace Swappy_V2
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kladr-css").Include(
+                "~/Scripts/kladr/jquery.kladr.min.css"));
         }
     }
 }
