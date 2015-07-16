010e7d6b746a678d101af718c8d752675a087a18
alikhil <alikhil@mail.ru>
2015-07-17 02:09:11 +0500

    добавлен расширитель функций для Exception. GetAllMessages - рекурсивно вытаскивает все сообщения

---
+++ Swappy-V2/Classes/ExceptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Classes
+{
+    public static class ExceptionExtensions
+    {
+        public static string GetAllMessages(this Exception e)
+        {
+            var messages = "";
+            var cur = e;
+            while (cur != null)
+            {
+                messages += cur.Message + '\n';
+                cur = cur.InnerException;
+            }
+            return messages;
+        }
+    }
+}
