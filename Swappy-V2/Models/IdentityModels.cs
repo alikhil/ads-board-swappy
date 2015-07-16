7f5b8043a082f6708a8bb7fc99f058159cd01eea
alikhil <alikhil@mail.ru>
2015-07-17 02:40:24 +0500

    Добавлены Claims и дополнительные поля в ApplicationUser

--- Swappy-V2/Models/IdentityModels.cs
+++ Swappy-V2/Models/IdentityModels.cs
@@ -9,11 +9,20 @@ namespace Swappy_V2.Models
     // Чтобы добавить данные профиля для пользователя, можно добавить дополнительные свойства в класс ApplicationUser. Дополнительные сведения см. по адресу: http://go.microsoft.com/fwlink/?LinkID=317594.
     public class ApplicationUser : IdentityUser
     {
+        public int AppUserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Здесь добавьте утверждения пользователя
+            userIdentity.AddClaim(new Claim("AppUserId",AppUserId.ToString()));
+            userIdentity.AddClaim(new Claim("Name", Name));
+            userIdentity.AddClaim(new Claim("City", City));
+            userIdentity.AddClaim(new Claim("Surname", Surname));
             return userIdentity;
         }
     }
