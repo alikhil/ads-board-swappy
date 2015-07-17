71714d42abe93affe85759d30bf7b35eeb183e88
alikhil <alikhil@mail.ru>
2015-07-18 02:55:35 +0500

    добавлен DataContext с AppUserModel

---
+++ Swappy-V2/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<AppUserModel> Users { get; set; }
+    }
+}
