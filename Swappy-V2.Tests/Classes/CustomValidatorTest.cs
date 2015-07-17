3f5d359426f10d87a74b5cae7f8963ae159f6c0d
alikhil <alikhil@mail.ru>
2015-07-18 02:40:18 +0500

    Создан класс теста CustomValidatorTest - и добавлена проверка работоспособности CityValid

---
+++ Swappy-V2.Tests/Classes/CustomValidatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Swappy_V2.Classes;
+using System.Threading.Tasks;
+
+namespace Swappy_V2.Tests
+{
+    [TestClass]
+    public class CustomValidatorTest
+    {
+        [TestMethod]
+        public void CityValid_Ufa_True()
+        {
+            //Arrange - Method is static
+
+            //Act
+            bool check = CustomValidator.CityValid("Уфа").Result;
+
+            //Assert
+            Assert.AreEqual(check, true);
+        }
+    }
+}
