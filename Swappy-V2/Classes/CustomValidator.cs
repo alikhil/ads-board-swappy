8e15709b5f29b2180222e195aa234ee44ce2e6c2
alikhil <alikhil@mail.ru>
2015-07-18 02:04:52 +0500

    Добавлен класс для валидации данных - CustomValidator. Функция проверки валидности города

---
+++ Swappy-V2/Classes/CustomValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Swappy_V2.Modules.KladrModule;
+namespace Swappy_V2.Classes
+{
+    public static class CustomValidator
+    {
+        public static async Task<bool> CityValid(string city)
+        {
+            KladrClient client = new KladrClient("", AppConstants.KladrId);
+            var resp = await client.Check(new Dictionary<string, string>
+                                        {
+                                            {"query", city },
+                                            {"contentType", "city"},
+                                            {"withParent", "1"}
+                                        });
+            return resp == city;
+        }
+    }
+}
