02279626e8d68ac95425d1e731e2384e11a16230
alikhil <alikhil@mail.ru>
2015-07-22 02:08:41 +0500

    Добавлен ускоритель ToList()

---
+++ Swappy-V2/Classes/IEnumerableExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Classes
+{
+    public static class IEnumerableExtentions
+    {
+        public static List<TSource> ToList<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (count < 0) throw new ArgumentOutOfRangeException("count");
+            var list = new List<TSource>(count);
+            foreach (var item in source)
+            {
+                list.Add(item);
+            }
+            return list;
+        }
+    }
+}
