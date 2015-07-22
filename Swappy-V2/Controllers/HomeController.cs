d1b6821fca970ed741a6bbcc2c5473d74e0a0ba4
alikhil <alikhil@mail.ru>
2015-07-23 01:39:07 +0500

    Убраны страницы About и Contact в HomeController

--- Swappy-V2/Controllers/HomeController.cs
+++ Swappy-V2/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace Swappy_V2.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
