cfafeb5c0fa47d60c43111367714a7b0416c7595
alikhil <alikhil@mail.ru>
2015-07-24 01:30:19 +0500

    Добавлены модели DealModel И ItemModel

---
+++ Swappy-V2/Models/DealModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Models
+{
+    public class DealModel
+    {
+        public int Id { get; set; }
+        public int AppUserModelId { get; set; }
+        public int? ItemToChangeId { get; set; }
+
+        [Required]
+        [Display(Name = "Вещь которую вы хотите обменять")]
+        public ItemModel ItemToChange { get; set; }
+
+        [Required]
+        [Display(Name = "Варианты обмена")]
+        public ICollection<ItemModel> Variants { get; set; }
+
+        [Required]
+        [Display(Name="Рассматрю другие варианты")]
+        public bool AnotherVariants { get; set; }
+
+        [Required]
+        [Display(Name="Город")]
+        public string City { get; set; }
+    }
+}
---
+++ Swappy-V2/Models/ItemModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Models
+{
+    public class ItemModel
+    {
+        public int Id { get; set; }
+        public int DealId { get; set; }
+
+        [Required]
+        [Display(Name="Название")]
+        [MaxLength(50, ErrorMessage = "{0} должно иметь не более {1} символов")]
+        public string Title { get; set; }
+
+        [Display(Name = "Примечание")]
+        [MaxLength(300, ErrorMessage = "{0} должно иметь не более {1} символов")]
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public DealModel Deal { get; set; }
+    }
+}
