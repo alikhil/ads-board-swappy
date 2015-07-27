08bd22c500522a4e6dadc24016127e62d970d49a
alikhil <alikhil@mail.ru>
2015-07-27 23:06:44 +0500

    Добавлена функция ImageValid в CustomValidator, возвращает ValidStatus

--- Swappy-V2/Classes/CustomValidator.cs
+++ Swappy-V2/Classes/CustomValidator.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using Swappy_V2.Modules.KladrModule;
+using System.Globalization;
+using System.IO;
 namespace Swappy_V2.Classes
 {
     public static class CustomValidator
@@ -19,5 +21,39 @@ namespace Swappy_V2.Classes
                                         });
             return resp == city;
         }
+
+        public static ValidStatus FileVaild(HttpPostedFileWrapper file)
+        {
+            List<string> errors = new List<string>();
+            ValidStatus result = ValidStatus.Unknown;
+            if (file != null && file.ContentLength > 0)
+            {
+                if (file.ContentLength > AppConstants.MaxImageLengthBytes)
+                    result |= ValidStatus.MaxLengthOverload | ValidStatus.NotValid;
+
+                if (!file.ContentType.ToLower(CultureInfo.InvariantCulture).StartsWith("image/"))
+                    result |= ValidStatus.IncorrectType | ValidStatus.NotValid;
+
+                var ext = Path.GetExtension(file.FileName);
+                if (AppConstants.AllowedImageExtensions.Contains(ext))
+                    result |= ValidStatus.IncorrectFormat| ValidStatus.NotValid;
+
+            }
+            else
+                result = ValidStatus.Empty;
+            result = (result & ValidStatus.NotValid) == ValidStatus.NotValid ? result : ValidStatus.Valid;
+            result ^= ValidStatus.Unknown;
+            return result;
+        }
+    }
+    public enum ValidStatus
+    {
+        Valid,
+        MaxLengthOverload,
+        IncorrectFormat,
+        IncorrectType,
+        NotValid,
+        Empty,
+        Unknown
     }
 }
