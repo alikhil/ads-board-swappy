fab2e927fcc5ac0e7fcdd8d48cc16631d9387626
alikhil <alikhil@mail.ru>
2015-07-30 01:58:13 +0500

    Добавлены Deals и Items в DataContext

--- Swappy-V2/Models/DataContext.cs
+++ Swappy-V2/Models/DataContext.cs
@@ -9,5 +9,7 @@ namespace Swappy_V2.Models
     public class DataContext : DbContext
     {
         public DbSet<AppUserModel> Users { get; set; }
+        public DbSet<ItemModel> Items { get; set; }
+        public DbSet<DealModel> Deals { get; set; }
     }
 }
