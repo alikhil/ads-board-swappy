522a7a5f43215ba23dd6a23d99d6a00a9d860a08
alikhil <alikhil@mail.ru>
2015-07-30 02:00:14 +0500

    Добавлен класс для JsonObjects - для использования в запросах

---
+++ Swappy-V2/Classes/JsonObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Classes
+{
+    public class JsonObject
+    {
+        public string Status { get; set; }
+        public object Result { get; set; }
+    }
+}
