11aee63bd832536c381f93cf123773b372fefaf0
alikhil <alikhil@mail.ru>
2015-07-30 01:59:22 +0500

    Добавлен вспомогательный класс - Util содержащий функцию загрузки файла на сервер - SaveFile()

---
+++ Swappy-V2/Classes/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Classes
+{
+    public static class Util
+    {
+        public static string SaveFile(string dir, string fname, HttpPostedFileWrapper file)
+        {
+
+            string ans = Path.Combine(Path.Combine(dir, fname));
+            dir = "~" + dir;
+            string path = Path.Combine(HttpContext.Current.Server.MapPath(dir), fname);
+            file.SaveAs(path);
+            return ans;
+        }
+    }
+}
