835eed3a79f8769dcbf1fd06f907493a3eaf3667
alikhil <alikhil@mail.ru>
2015-08-27 18:50:00 +0300

    Добавлен репозиторий для Items. ItemsRepository

--- Swappy-V2/Models/Repository.cs
+++ Swappy-V2/Models/Repository.cs
@@ -78,4 +78,64 @@ namespace Swappy_V2.Models
             GC.SuppressFinalize(this);
         }
     }
+
+    /// <summary>
+    /// Обертка для работы с таблицей объявлений
+    /// </summary>
+    public class ItemsRepository : IRepository<ItemModel>
+    {
+        private bool disposed = false;
+
+        DataContext db = new DataContext();
+        public List<ItemModel> GetList()
+        {
+            var list = db.Items;
+            return list.ToList();
+        }
+
+        public void Create(ItemModel item)
+        {
+            db.Items.Add(item);
+        }
+
+        public void Update(ItemModel item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            var item = db.Items.Find(id);
+            db.Items.Remove(item);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public ItemModel Get(int id)
+        {
+            return db.Items.Find(id);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
 }
