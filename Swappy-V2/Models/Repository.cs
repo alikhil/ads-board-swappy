cea6d5b80073e51884951602e0bc89d2b15e218d
alikhil <alikhil@mail.ru>
2015-08-27 13:24:47 +0300

    Создан интерфейс IRepository

---
+++ Swappy-V2/Models/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swappy_V2.Models
+{
+    public interface IRepository<Type>
+    {
+        List<Type> GetList();
+        Type GetComputer(int id);
+        void Create(Type item);
+        void Update(Type item);
+        void Delete(int id);
+        void Save();
+    }
+}
