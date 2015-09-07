7ae31f3d57a221bbd71b37f5c01e5bfebafb16c9
alikhil <alikhil@mail.ru>
2015-09-07 20:59:26 +0300

    Добавлена вспомогательные классы для удобного использования моков

---
+++ Swappy-V2/Classes/MockingHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Principal;
+using System.Web;
+namespace Swappy_V2.Classes
+{
+    public class MockingHelper : Mockable
+    {
+        public int GetAppUserId(IIdentity identity)
+        {
+            return identity.GetAppUserId();
+        }
+
+        public string GetClaim(IIdentity identity, string p)
+        {
+            return identity.GetClaim(p);
+        }
+    }
+    public interface Mockable
+    {
+        int GetAppUserId(IIdentity identity);
+        string GetClaim(IIdentity identity, string p);
+    }
+    
+    public interface IPathProvider
+    {
+        string MapPath(string path);
+    }
+
+    public class ServerPathProvider : IPathProvider
+    {
+        public string MapPath(string path)
+        {
+            return HttpContext.Current.Server.MapPath(path);
+        }
+    }
+}
