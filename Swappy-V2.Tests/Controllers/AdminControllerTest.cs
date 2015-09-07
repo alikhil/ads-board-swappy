8238750bbecdc6e0e1462efaadf9e7c4e68deb97
alikhil <alikhil@mail.ru>
2015-09-07 22:17:55 +0300

    Создан класс тестов для adminController

---
+++ Swappy-V2.Tests/Controllers/AdminControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Swappy_V2.Tests.Controllers
+{
+    [TestClass]
+    public class AdminControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
