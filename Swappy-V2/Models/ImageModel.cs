2645f19559561781444026c5c20707cfa7a58fa9
alikhil <alikhil@mail.ru>
2015-09-27 10:50:39 +0300

    Добавлена модель ImageModel

---
+++ Swappy-V2/Models/ImageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Swappy_V2.Models
+{
+    public class ImageModel
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+
+        public DealModel Deal { get; set; }
+    }
+}
